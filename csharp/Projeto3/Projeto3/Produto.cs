a8a316404a378f5723bb2c4e13c13a22c23c67ee
Gladson <gladson.ramos@hotmail.com>
2022-05-05 16:35:32 -0400

    Projeto 3 - Modulos {menu,listar,incluir} ok

---
+++ csharp/Projeto3/Projeto3/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projeto3
+{
+    [System.Serializable]
+    internal class Produto
+    {
+        protected string _nome;
+        protected float _preco;
+        protected float _peso;
+        protected float _volume;
+
+        public Produto(string nome, float preco, float peso, float volume)
+        {
+            _nome = nome;
+            _preco = preco;
+            _peso = peso;
+            _volume = volume;
+
+        }
+        public override string ToString()
+        {
+            return $"Nome: {_nome}\nPreço: R$ {_preco}\nPeso: {_peso} KG\nVolume: {_volume} metros cubicos";
+        }
+    }
+}
--- csharp/Projeto3/Projeto3/Program.cs
+++ csharp/Projeto3/Projeto3/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace Projeto3
 {
@@ -13,14 +16,47 @@ namespace Projeto3
 
     internal class Program
     {
-        #region Funções do Programa
 
+        #region Manipulação de dados
         static void CarregaArquivo()
         {
+            FileStream arquivo = new FileStream("Produtos.prd", FileMode.OpenOrCreate);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                produtos = (List<Produto>)formatter.Deserialize(arquivo);
+
+                if (produtos == null)
+                {
+                    produtos = new List<Produto>();
+                }
+            }
+            catch (Exception ex)
+            {
+                produtos = new List<Produto>();
+            }
+            arquivo.Close();
 
         }
+        static void Salvar()
+        {
+            FileStream arquivo = new FileStream("Produtos.prd", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(arquivo, produtos);
+
+            arquivo.Close();
+
+        }
+
+        #endregion
+
+
+        #region Funções do Programa
         static void MostraMenu()
         {
+            Titulo("Sistema de cadastro de produtos");
             int i = 1;
             foreach (string menu in Enum.GetNames(typeof(Menu)))
             {
@@ -43,27 +79,64 @@ namespace Projeto3
         }
 
         #region Funções Cadastro de Produtos
+        
         static void Listar()
         {
-            Console.WriteLine("Listar");
-            Espera();
+            Titulo("Lista de Produtos");
+            foreach (Produto item in produtos)
+            {
+                Console.WriteLine(item);
+                Linha();
+            }
+            
+            
         }
 
         static void IncluirProduto()
         {
-            Console.WriteLine("Incluindo Produto");
-            Espera();
+            bool maisProdutos = true;
+            while (maisProdutos)
+            {
+                Console.WriteLine("Incluindo Produto");
+                Espera();
+                Console.Write("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o preço do produto: ");
+                float preco = float.Parse(Console.ReadLine());
+                Console.Write("Digite o peso do produto: ");
+                float peso = float.Parse(Console.ReadLine());
+                Console.Write("Digite o volume do produto: ");
+                float volume = float.Parse(Console.ReadLine());
+                Produto produto = new Produto(nome, preco, peso, volume);
+                produtos.Add(produto);
+                Salvar();
+                Console.WriteLine("Deseja incluir mais produtos?[sim/nao]");
+                string simNao = Console.ReadLine();
+                if (simNao == "sim")
+                {
+                }
+                else
+                {
+                    maisProdutos=false;
+                }
+                Espera();
+            }
+            Salvar();
+            Console.WriteLine("Produtos incluidos com sucesso");
+            
         }
 
         static void AlterarProduto()
         {
             Console.WriteLine("Alterar Produto");
+
             Espera();
         }
 
         static void ExcluirProduto()
         {
-            Console.WriteLine("Excluir Produto");
+            Titulo("Excluir Produto");            
+            
             Espera();
         }
 
@@ -100,33 +173,41 @@ namespace Projeto3
         {
             Console.Clear();
         }
-        static void Titulo()
+        static void Titulo(string titulo)
         {
-            Console.WriteLine("Sistema de estoque");
+            Linha();
+            Console.WriteLine(titulo);
+            Linha();
         }
         static void Linha()
         {
-            Console.WriteLine("========================");
+            Console.WriteLine("===============================");
 
         }
         #endregion
+        
+        #region variaveis globais
+        static List<Produto> produtos = new List<Produto>();
         enum Menu { Listar = 1, Incluir, Alterar, Excluir, Comprar, Vender, Sair }
+        #endregion
+
         static void Main(string[] args)
         {
             CarregaArquivo();
             bool escolheuFicar = true;
             while (escolheuFicar)
-            {
-                Linha();
-                Titulo();
-                Linha();
+            {   
                 MostraMenu();
+                Linha();
                 int opcao = RecebeOpção();
                 switch ((Menu)opcao)
                 {
 
                     case Menu.Listar:
                         Listar();
+                        Console.WriteLine("Pressione enter tecla para sair");
+                        Console.ReadLine();
+                        Espera();
                         break;
                     
                     case Menu.Incluir:
@@ -138,6 +219,12 @@ namespace Projeto3
                         break;
 
                     case Menu.Excluir:
+                        Console.WriteLine("Deseja listar os produtos?");
+                        string simNao=Console.ReadLine();
+                        if (simNao == "sim")
+                        {
+                            Listar();
+                        }
                         ExcluirProduto();
                         break;
 
