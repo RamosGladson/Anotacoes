426f0e75908a169ea2514d93079c16c605f6dbc6
Ubuntu <ubuntu@ip-10-30-4-120.ec2.internal>
2022-10-26 19:37:13 +0000

    update

---
+++ myLab/terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "Define the instance region"
+  default = "us-east-1"
+}
+
+variable "name" {
+  description = "Application's name"
+  default = "Server 01"
+}
+
+variable "env" {
+  description = "Application's
+
