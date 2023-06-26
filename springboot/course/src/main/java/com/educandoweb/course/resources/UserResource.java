1e92f0317a3adb0f0f663b2a3b8630afc697ffd3
Gladson <gladson.ramos@hotmail.com>
2023-06-26 05:26:39 -0400

    :recycle: refactor: User End Point
    User add end point refactor
    
    by Gladson Carneiro Ramos

--- springboot/course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ springboot/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,10 +1,10 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -31,7 +32,10 @@ public class UserResource {
 
     @PostMapping("/add")
     public ResponseEntity<User> addUser(@RequestBody User user) {
-        return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
+        user = userService.addUser(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
     @GetMapping("/{id}")
